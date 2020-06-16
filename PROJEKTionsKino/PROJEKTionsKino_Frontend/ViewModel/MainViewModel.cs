a5b6f10bde7a4b761cafb096ab92867a8ff47205
Laura Legat <laura.legat11@gmail.com>
2020-06-16 22:25:07 +0200

    push

--- PROJEKTionsKino/PROJEKTionsKino_Frontend/ViewModel/MainViewModel.cs
+++ PROJEKTionsKino/PROJEKTionsKino_Frontend/ViewModel/MainViewModel.cs
@@ -11,21 +11,22 @@ namespace PROJEKTionsKino_Frontend.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        public object test { get; set; }
+        #region Ticket kaufen
+        public string Notif { get; set; }
+        public RelayCommand TicketKaufenBtnClickedCmd { get; set; }
 
-        public string Vorname { get; set; } = "TotallyNotLaura";
-        public string Nachname { get; set; } = "Legat";
-        public string Strasse { get; set; } = "SomeStreet";
-        public string Stadt { get; set; } = "SomeCity";
-        public int Hausnr { get; set; } = 23;
-        public int PLZ { get; set; } = 1190;
+        public ObservableCollection<Film> Filme { get; set; } 
+        #endregion
 
-        //public string Vorname { get; set; }
-        //public string Nachname { get; set; }
-        //public string Strasse { get; set; }
-        //public string Stadt { get; set; }
-        //public int Hausnr { get; set; }
-        //public int PLZ { get; set; }
+
+        #region Kunde anlegen
+        public string Vorname { get; set; } 
+        public string Nachname { get; set; }
+        public string Strasse { get; set; }
+        public string Stadt { get; set; }
+        public int Hausnr { get; set; }
+        public int PLZ { get; set; }
+
         public DateTime Erstelldatum { get; set; }
         public DateTime Geburtstag { get; set; }
         public bool WantsVK { get; set; }
@@ -38,17 +39,21 @@ namespace PROJEKTionsKino_Frontend.ViewModel
         public ObservableCollection<Kunde> Kunden
         {
             get => kunden;
-            set { kunden = value; RaisePropertyChanged(); }
+            set { kunden = value; }
         }
+        #endregion
+
+
         public OracleConnection DbConnection = new OracleConnection
         {
             ConnectionString =
                 "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=infdb.technikum-wien.at)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=O10)));User Id=s20bwi4_wi18b055;Password=dbss20;"
         };
 
-    public MainViewModel()
+        public MainViewModel()
         {
             Kunden = new ObservableCollection<Kunde>();
+
             AddCustomerClickedCmd = new RelayCommand(
                 () =>
                 {
@@ -70,12 +75,18 @@ namespace PROJEKTionsKino_Frontend.ViewModel
 
                     GetKunden();
 
-                    
+
                 }, () => canAdd);
+
+            TicketKaufenBtnClickedCmd = new RelayCommand(
+                () =>
+                {
+
+                });
 
             if (!IsInDesignMode)
             {
-                
+
 
                 GetKunden();
                 canAdd = true;
@@ -106,6 +117,5 @@ namespace PROJEKTionsKino_Frontend.ViewModel
                 var test = 4;
             }
         }
-
     }
 }
