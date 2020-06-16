c311ca51d3e6e8483d81577d7da827ffe841da9d
Laura Legat <laura.legat11@gmail.com>
2020-06-16 15:46:57 +0200

    push

--- PROJEKTionsKino/PROJEKTionsKino_Frontend/Model/Kunde.cs
+++ PROJEKTionsKino/PROJEKTionsKino_Frontend/Model/Kunde.cs
@@ -8,12 +8,25 @@ namespace PROJEKTionsKino_Frontend.Model
 {
     public class Kunde
     {
+        public Kunde(string vorname, string nachname, string straße, int hausNr, int pLZ, DateTime geburtsdatum, DateTime erstelldatum, bool isVorteilskunde)
+        {
+            Vorname = vorname;
+            Nachname = nachname;
+            Straße = straße;
+            HausNr = hausNr;
+            PLZ = pLZ;
+            Geburtsdatum = geburtsdatum;
+            Erstelldatum = DateTime.Now;
+            IsVorteilskunde = isVorteilskunde;
+        }
+
         public string Vorname { get; set; }
         public string Nachname { get; set; }
         public string Straße { get; set; }
         public int HausNr { get; set; }
         public int PLZ { get; set; }
         public DateTime Geburtsdatum { get; set; }
+        public DateTime Erstelldatum { get; set; }
         public bool IsVorteilskunde { get; set; }
     }
 }
