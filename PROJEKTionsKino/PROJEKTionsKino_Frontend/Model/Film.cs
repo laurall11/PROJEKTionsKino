bf840f2a0d7f2f1e4c404a6bffd1d3a3a28b9ca9
Doenerlord <50668362+Doenerlord@users.noreply.github.com>
2020-06-16 22:27:13 +0200

    ADD FILM

---
+++ PROJEKTionsKino/PROJEKTionsKino_Frontend/Model/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJEKTionsKino_Frontend.Model
+{
+    public class Film
+    {
+        public int FilmID { get; set; }
+        public int Dauer { get; set; }
+        public int Altersfreigabe { get; set; }
+        public int Erscheinungsjahr { get; set; }
+        public int Ratinganzahl { get; set; }
+        public int Ratingsterne { get; set; }
+        public string Filmname { get; set; }
+        public string Beschreibung { get; set; }
+        public string Genre { get; set; }
+        public string Regie { get; set; }
+    }
+}
