b501bc3be03e00cdcb0d4ccc9c46f061aea9d742
Doenerlord <50668362+Doenerlord@users.noreply.github.com>
2020-06-17 19:45:45 +0200

    dsfsdf

--- PROJEKTionsKino/PROJEKTionsKino_Frontend/Model/Sitzplatz.cs
+++ PROJEKTionsKino/PROJEKTionsKino_Frontend/Model/Sitzplatz.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace PROJEKTionsKino_Frontend.Model
 {
-    class Sitzplatz
+    public class Sitzplatz
     {
+        public int SitzplatzID { get; set; }
+        public int SitzpatzkategorieID { get; set; }
+        public int SaalID { get; set; }
+        public int Sitzplatznummer { get; set; }
+        public int Reihe { get; set; }
+
+        public Sitzplatz(int sitzplatzID, int sitzpatzkategorieId, int saalId, int sitzplatznummer, int reihe)
+        {
+            SitzpatzkategorieID = sitzpatzkategorieId;
+            SitzplatzID = sitzplatzID;
+            SaalID = saalId;
+            Sitzplatznummer = sitzplatznummer;
+            Reihe = reihe;
+        }
     }
 }
+//sitzplatzid INT, sitzplatzkategorieid INT, saalid INT, sitzplatznr INT, reihe int
