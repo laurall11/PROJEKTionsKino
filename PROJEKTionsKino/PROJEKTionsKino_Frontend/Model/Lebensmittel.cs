1b3e794672c12458603de7537d803ff2db7ad37e
Doenerlord <50668362+Doenerlord@users.noreply.github.com>
2020-06-25 16:12:00 +0200

    gsf

---
+++ PROJEKTionsKino/PROJEKTionsKino_Frontend/Model/Lebensmittel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJEKTionsKino_Frontend.Model
+{
+    public class Lebensmittel
+    {
+        //LebensmittelID INT, Name string, Preis double, kategorie string
+        public int LebensmittelID { get; set; }
+        public string Name { get; set; }
+        public double Preis { get; set; }
+        public string Kategorie { get; set; }
+    }
+}
